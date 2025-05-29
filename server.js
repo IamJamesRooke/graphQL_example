ed160931ac16bec1f6a88af6c72bc689823968bc
IamJamesRooke <me@jamesrooke.blog>
2025-05-29 18:05:31 -0500

    basic schema for e-commerce

--- server.js
+++ server.js
@@ -4,15 +4,70 @@ const { createYoga } = require('graphql-yoga')
 
 const schema = buildSchema(`
     type Query {
-        description: String
-        price: Float
-    }    
+        products: [Product]
+        orders: [Order]
+    } 
+        
+    type Product {
+        id: ID!
+        description: String!
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review {
+        rating: Int!
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: []
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!    
+    }
 `);
+
+const root = {
+    products: [
+        {
+            id: 'redshoe',
+            description: 'Red Shoe',
+            price: 42.42
+        },
+        {
+            id: 'bluejean',
+            description: 'Blue Jeans',
+            price: 55.55
+        }
+    ],
+    orders: [
+        {
+            date: '2005-05-05',
+            subtotal: 90.22,
+            items: [
+                {
+                    product: {
+                        id: 'redshoe',
+                        description: 'Old Red Shoe',
+                        price: 45.11
+                    },
+                    quantity: 2
+                }
+            ]
+        }
+    ]
+}
 
 const app = express();
 
 app.use('/graphql', createYoga({
-    schema: schema
+    schema: schema,
+    graphiql: true,
 }));
 
 app.listen(3000, () => {
