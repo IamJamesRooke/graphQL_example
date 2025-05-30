14e7fe60879b11b70c95665602cd5323f1c63bb2
IamJamesRooke <me@jamesrooke.blog>
2025-05-29 19:15:38 -0500

    modularizing with graphql tools

---
+++ products/products.model.js
@@ -0,0 +1,14 @@
+module.exports = {
+    products: [
+        {
+            id: 'redshoe',
+            description: 'Red Shoe',
+            price: 42.42
+        },
+        {
+            id: 'bluejean',
+            description: 'Blue Jeans',
+            price: 55.55
+        }
+    ]
+}
